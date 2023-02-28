a998e14c0815f75dd7893fa817e726c7616fd1c7
Theo Browne <me@t3.gg>
2023-02-27 19:18:36 -0800

    Add clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,48 @@
-import { type AppType } from "next/app";
-
-import { api } from "~/utils/api";
-
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/nextjs";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+//  List pages you want to be publicly accessible, or leave empty if
+//  every page requires authentication. Use this naming strategy:
+//   "/"              for pages/index.js
+//   "/foo"           for pages/foo/index.js
+//   "/foo/bar"       for pages/foo/bar.js
+//   "/foo/[...bar]"  for pages/foo/[...bar].js
+const publicPages: Array<string> = ["/", "/about"];
 
-export default api.withTRPC(MyApp);
+function MainApp({ Component, pageProps }: AppProps) {
+  // Get the pathname
+  const { pathname } = useRouter();
+
+  // Check if the current route matches a public page
+  const isPublicPage = publicPages.includes(pathname);
+
+  // If the current route is listed as public, render it directly
+  // Otherwise, use Clerk to require authentication
+  return (
+    <ClerkProvider>
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <div>Hi</div>
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
+}
+
+// Wrap with tRPC
+import { api } from "~/utils/api";
+export default api.withTRPC(MainApp);
