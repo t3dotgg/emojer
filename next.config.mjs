35d1d4d110c96a3e9295715353ef424a2d01e893
Theo Browne <me@t3.gg>
2023-02-28 23:12:38 -0800

    Don't block on ts on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -23,5 +23,7 @@ const config = {
   //   locales: ["en"],
   //   defaultLocale: "en",
   // },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 export default config;
