82a18eeb1c17806e2789e31c45e83e39a4bbbff4
Theo Browne <me@t3.gg>
2023-02-28 23:45:58 -0800

    add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { Suspense } from "react";
 
 dayjs.extend(relativeTime);
 
@@ -10,8 +11,10 @@ const Feed = () => {
   return (
     <div className="flex h-full w-full grow flex-col border-l border-r border-zinc-700 md:w-[600px]">
       <CreatePostWizard />
-      {/* @ts-expect-error Server Component */}
-      <Tweets />
+      <Suspense>
+        {/* @ts-expect-error Server Component */}
+        <Tweets />
+      </Suspense>
     </div>
   );
 };
