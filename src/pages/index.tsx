3f3d7aeb238ec71fe970fb2bb629958e0fc2220d
Theo Browne <me@t3.gg>
2023-03-08 00:02:45 -0800

    readme + cleanup starting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -80,12 +80,14 @@ const CreatePostWizard = () => {
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
   const { isLoaded: userLoaded } = useUser();
+
   if (postsLoading || !userLoaded)
     return (
       <div className="absolute flex h-screen w-screen items-center justify-center">
         <Loading size={128} />
       </div>
     );
+
   return (
     <div className="flex h-full w-full grow flex-col border-l border-r border-zinc-700 md:w-[600px]">
       <CreatePostWizard />
