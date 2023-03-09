940cc33b2ac0d78d1f7ed0e11c033eea54b883d0
Theo Browne <me@t3.gg>
2023-03-08 23:01:06 -0800

    use official emoji support

--- src/shared/emojiValidator.ts
+++ src/shared/emojiValidator.ts
@@ -1,11 +1,5 @@
 import { z } from "zod";
 
-export const emojiRegex = /^(\p{Extended_Pictographic}|\p{Emoji_Component})+$/u;
-
 export const emojiValidator = z.object({
-  message: z
-    .string()
-    .refine((value) => emojiRegex.test(value) && value.length < 100, {
-      message: "Must be an emoji",
-    }),
+  message: z.string().emoji(),
 });
